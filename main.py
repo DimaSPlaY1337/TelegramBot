10c0306132141f45c4abd62d273b21a64f5d4782
dimasplay1337@gmail.com <dimasplay1337@gmail.com>
2025-10-07 18:11:37 +0300

    Telegram

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ from src.common import bot
 
 if __name__ == '__main__':
     asyncio.run(bot.polling())
+    # Метод polling, например
+    # в aiogram или python - ботах, запускает непрерывный цикл опроса событий(например, новых сообщений от пользователей, команд и других апдейтов из внешнего API, например Telegram).
