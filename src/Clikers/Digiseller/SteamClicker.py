98639084b910bda3a1a51b6de8c80e1a1861b6c3
dimasplay1337@gmail.com <dimasplay1337@gmail.com>
2025-10-12 21:20:15 +0300

    Digiseller added

--- src/Clikers/Digiseller/SteamClicker.py
+++ src/Clikers/Digiseller/SteamClicker.py
@@ -17,7 +17,7 @@ class SteamClicker(PlatformClicker):
 
         os.startfile("C:\\Program Files\\Rockstar Games\\Launcher\\LauncherPatcher.exe")
         # win_rock = await wait_for_open("Rockstar Games - Sign In")
-        time.sleep(5)
+        time.sleep(10)
         # if win_rock:
         #     win_rock.minimize()
 
