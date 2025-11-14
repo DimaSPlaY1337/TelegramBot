cc0b108bfcdf9fe37e138d4df43764e7a810f100
dima <dimasplay1337@gmail.com>
2025-11-14 19:43:15 +0300

    Telegram bug fix 1

--- src/Clikers/Telegram/RockstarClicker.py
+++ src/Clikers/Telegram/RockstarClicker.py
@@ -137,7 +137,7 @@ class RockstarClicker(PlatformClicker):
             else:
                 print("Ошибка выбора версии GTA")
 
-            await self.rockstar_acceptance(messge, False)
+            await self.rockstar_acceptance(message, False)
             # win_rock = await wait_for_open("Rockstar Games Launcher", 10)
 
             win_gta = await wait_for_open("Grand Theft Auto V", 200)
